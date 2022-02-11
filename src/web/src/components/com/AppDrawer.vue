d39f9818df4cf2cd6cde0ed955fb80a868be3f5d
yuklynn <59094342+yuklynn@users.noreply.github.com>
2022-02-12 02:47:49 +0900

    メニュー追加

--- src/web/src/components/com/AppDrawer.vue
+++ src/web/src/components/com/AppDrawer.vue
@@ -1,5 +1,32 @@
 <template>
   <v-navigation-drawer app clipped>
-    <div>めにゅーとか</div>
+    <v-list nav>
+      <v-list-item v-for="item in menus" :key="item.label" link :to="item.link">
+        <v-list-item-icon class="mr-4">
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>{{ item.label }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
   </v-navigation-drawer>
 </template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+import { mdiAccountCircle, mdiBell, mdiHome, mdiTune } from '@mdi/js';
+
+export default defineComponent({
+  setup() {
+    const menus = [
+      { icon: mdiHome, label: 'ホーム', link: '/home' },
+      { icon: mdiBell, label: 'お知らせ', link: '/notification' },
+      { icon: mdiAccountCircle, label: 'プロフィール', link: '/profile' },
+      { icon: mdiTune, label: '設定', link: '/setting' },
+    ];
+
+    return { menus };
+  },
+});
+</script>
