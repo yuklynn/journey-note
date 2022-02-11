99d729073c2eff855f357abb93a6767d097dea07
yuklynn <59094342+yuklynn@users.noreply.github.com>
2022-02-11 22:24:01 +0900

    検索バー

--- src/web/src/views/Home.vue
+++ src/web/src/views/Home.vue
@@ -1,9 +1,15 @@
 <template>
-  <div>ほーむ</div>
+  <v-container>
+    <search-bar />
+    <div>いちらん</div>
+  </v-container>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api';
+import SearchBar from '@/components/com/SearchBar.vue';
 
-export default defineComponent({});
+export default defineComponent({
+  components: { SearchBar },
+});
 </script>
